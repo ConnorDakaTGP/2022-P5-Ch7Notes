afe544ef5093022edffd19d15453a24752fc1ef8
Connor Donegan <connor.donegan@vtsd.com>
2022-11-29 13:11:44 -0500

    07notes

--- VoidReturn.java
+++ VoidReturn.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
 public class VoidReturn {
+   public static void main(String[] args){
+      int total = addNums();
 
-   public static void main(String args[]){
-       
-
+      System.out.println("Your total was: "+total);
    }
+   public static int addNums(){
 
+      /*
+   Static: Non-object class, does the same thing no matter what (It is static)
+   Void: Does not send information to store
+    */
+
+      System.out.println("We are going to add two numbers");
+      System.out.println("Please add them below");
+
+      Scanner scan = new Scanner(System.in);
+      int num1 = scan.nextInt();
+      int num2 = scan.nextInt();
+
+      return (num1+num2);
+   }
 }
